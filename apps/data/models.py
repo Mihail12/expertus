139887fdd575577bdb5ed8b3ffff3b5abc7b1ef9
Mihail12 <spasenkomihail@gmail.com>
2018-04-04 01:22:13 +0300

    q

--- apps/data/models.py
+++ apps/data/models.py
@@ -3,3 +3,29 @@ from django.db import models
 
 class AppUser(AbstractUser):
     pass
+
+class Project(models.Model):
+    name = models.CharField(max_length=1)
+    type = models.CharField(max_length=1)
+    end_date = models.DateField()
+    interface_config = models.CharField(max_length=1)
+    description = models.TextField(max_length=4000)
+    active = models.BooleanField()
+    group = models.ForeignKey('ProjectGroup', on_delete=models.CASCADE)
+    classification = models.ManyToManyField('Classification')
+
+
+class ProjectGroup(models.Model):
+    name = models.CharField(max_length=1)
+    display_order = models.IntegerField()
+    status = models.CharField(max_length=1)
+    is_public = models.BooleanField()
+
+class Classification(models.Model):
+    strict = models.BooleanField()
+    structure = models.CharField(max_length=1)
+    multivalue = models.BooleanField()
+    user = models.BooleanField()
+    document = models.BooleanField()
+
+
